2eb0221871c7da90b93f18af94b8a4779d1580c3
69Faker <thiagogamarra82@gmail.com>
2025-07-23 15:58:11 -0700

    .

---
+++ exercicioJava/ControleCandidatos/src/candidatura/Main.java
@@ -0,0 +1,11 @@
+package candidatura;
+
+import static candidatura.ProcessoSeletivo.selecaoCandidatos;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        selecaoCandidatos();
+    }
+}
