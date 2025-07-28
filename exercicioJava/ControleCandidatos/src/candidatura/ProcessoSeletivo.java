c213a8834f74ada5d567fad0d932bf8c3156b0c9
69Faker <thiagogamarra82@gmail.com>
2025-07-28 16:11:45 -0700

    .
    Aprendendo fluxos de controle

--- exercicioJava/ControleCandidatos/src/candidatura/ProcessoSeletivo.java
+++ exercicioJava/ControleCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,8 +1,66 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo
 {
+
+    static void entrandoEmContato(String candidato)
+    {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do
+        {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+            {
+                tentativasRealizadas++;
+            }
+            else
+            {
+                System.out.println("Contato realizado com sucesso");
+            }
+
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+        {
+            System.out.println("Conseguimos contato com " + candidato + " Na " + tentativasRealizadas);
+        }
+        else
+        {
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas" + tentativasRealizadas + " realizada");
+        }
+    }
+
+    static boolean atender ()
+    {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados()
+    {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice++)
+        {
+            System.out.println("O candidato de n°" + (indice+1) + " é o " + candidatos[indice]);
+        }
+
+        /*System.out.println("forma abreviada de interação for each");
+          for(String candidatos: candidatos)
+          {
+            System.out.println("O candidato selecionado foi " + candidato);
+          }
+
+
+        */
+    }
+
     static void selecaoCandidatos()
     {
         String [] candidatos ={"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
