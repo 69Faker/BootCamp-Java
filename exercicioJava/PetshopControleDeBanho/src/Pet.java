21b7f2a41d7e96756de325b646a75edd44614333
69Faker <thiagogamarra82@gmail.com>
2025-07-31 16:11:21 -0700

    Abstração e Encapsulamento

---
+++ exercicioJava/PetshopControleDeBanho/src/Pet.java
@@ -0,0 +1,13 @@
+public class Pet
+{
+
+    private final String name;
+
+    private boolean clean;
+
+    public Pet(String name, boolean clean) {
+        this.name = name;
+        this.clean = clean;
+    }
+}
+
